9da982d504c0b4efd3940c6194741d28ab3d0606
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-14 01:54:30 +0600

    Creating a Stack using a Linked List: commit

--- stack.py
+++ stack.py
@@ -23,42 +23,114 @@ print("isEmpty:",isEmpty)
 
 #size
 print("Size:",len(stack))
-"""
+
 
 #creating a stack using class
 class Stack:
-  def __init__(self):
-    self.stack = []
-
-  def push(self, element):
-    self.stack.append(element)
-
-  def pop(self):
-    if self.isEmpty():
-      return "Stack is empty"
-    return self.stack.pop()
-
-  def peek(self):
-    if self.isEmpty():
-      return "Stack is empty"
-    return self.stack[-1]
-
-  def isEmpty(self):
-    return len(self.stack) == 0
-
-  def size(self):
-    return len(self.stack)
-
-# Create a stack
+    def __init__(self):
+        self.stack=[]
+    
+    def push(self,element):
+    
+     self.stack.append(element)
+     
+    def pop(self):
+       if self .isEmpty():
+            return "Stack is empty"
+       return self.stack.pop()
+    
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack[-1]
+    def isEmpty(self):
+        return len(self.stack) ==0
+    
+    def size(self):
+        return len(self.stack)==0
+    
+    def size(self):
+        return len(self.stack)
+        
 myStack = Stack()
 
 myStack.push('A')
 myStack.push('B')
 myStack.push('C')
 
-print("Stack: ", myStack.stack)
-print("Pop: ", myStack.pop())
-print("Stack after Pop: ", myStack.stack)
-print("Peek: ", myStack.peek())
-print("isEmpty: ", myStack.isEmpty())
-print("Size: ", myStack.size())
+print("Stack:",myStack.stack)
+print("pop:",myStack.pop())
+print("Stack after Pop:",myStack.stack)
+
+print("Peek:",myStack.peek())
+print("isEmpty:",myStack.isEmpty())
+print("Size:",myStack.size())
+
+"""
+
+
+#Creating a Stack using a Linked List
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.size= 0
+    
+    def push(self,value):
+        new_node = Node(value)
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
+        self.size +=1
+
+
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        pooped_node = self.head
+        self.head = self.head.next
+        self.size -=1
+        return pooped_node.value
+    
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.head.value
+    
+    def isEmpty(self):
+        return self.size == 0
+    
+    def stackSize(self):
+        return self.size
+    
+    def traverseAndPrint(self):
+        currentNode = self.head
+        while currentNode:
+            print(currentNode.value,end="->")
+
+            currentNode =currentNode.next
+            print()
+
+myStack = Stack()
+myStack.push('A')
+myStack.push('B')
+myStack.push('C')
+
+print("LinkedList:",end="")
+myStack.traverseAndPrint()
+
+print("Peaek :",myStack.peek())
+print("Pop:",myStack.pop())
+print("LinkedList after Pop:",end="")
+
+myStack.traverseAndPrint()
+
+print("isEmpty:",myStack.isEmpty())
+
+print("Size:",myStack.stackSize())
+
