751812367c9cc1ddcc43da49f79e03ecaf265b9e
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-15 22:55:34 +0600

    Using a Python class as a queue

--- queues.py
+++ queues.py
@@ -75,7 +75,7 @@ class Queue:
       temp = temp.next
     print()
 
-    """
+    
 # Create a queue
 
 myQueue = Queue()
@@ -92,3 +92,45 @@ print("Queue after Dequeue: ", end="")
 myQueue.printQueue()
 print("isEmpty: ", myQueue.isEmpty())
 print("Size: ", myQueue.size())
+
+"""
+
+#Using a Python class as a queue
+
+class Queue:
+  def __init__(self):
+    self.queue = []
+    
+  def enqueue(self, element):
+    self.queue.append(element)
+
+  def dequeue(self):
+    if self.isEmpty():
+      return "Queue is empty"
+    return self.queue.pop(0)
+
+  def peek(self):
+    if self.isEmpty():
+      return "Queue is empty"
+    return self.queue[0]
+
+  def isEmpty(self):
+    return len(self.queue) == 0
+
+  def size(self):
+    return len(self.queue)
+
+# Create a queue
+myQueue = Queue()
+
+myQueue.enqueue('A')
+myQueue.enqueue('B')
+myQueue.enqueue('C')
+
+print("Queue: ", myQueue.queue)
+print("Peek: ", myQueue.peek())
+print("Dequeue: ", myQueue.dequeue())
+print("Queue after Dequeue: ", myQueue.queue)
+print("isEmpty: ", myQueue.isEmpty())
+print("Size: ", myQueue.size())
+
