4c3302ecdf797eb528ab5116e5a8b5840bc77281
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-15 23:50:03 +0600

    Finding the lowest value in a singly linked list in Python commit

--- Linked Lists with .py
+++ Linked Lists with .py
@@ -1,4 +1,5 @@
-#Traversal of a singly linked list in Python
+"""
+#TTraversal of a singly linked list in Python:
 class Node:
   def __init__(self, data):
     self.data = data
@@ -23,3 +24,32 @@ node3.next = node4
 node4.next = node5
 
 traverseAndPrint(node1)
+"""
+
+#Finding the lowest value in a singly linked list in Python
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+def findLowestValue(head):
+  minValue = head.data
+  currentNode = head.next
+  while currentNode:
+    if currentNode.data < minValue:
+      minValue = currentNode.data
+    currentNode = currentNode.next
+  return minValue
+
+node1 = Node(7)
+node2 = Node(11)
+node3 = Node(3)
+node4 = Node(2)
+node5 = Node(9)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+print("The lowest value in the linked list is:", findLowestValue(node1))
