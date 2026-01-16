631080b354289159360c3d4fe8c261674ce13a57
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-16 23:40:08 +0600

    add item in has function commit

--- Hash Tables with.py
+++ Hash Tables with.py
@@ -1,5 +1,5 @@
 #Create a Hash Function that sums the Unicode numbers of each character and return a number between 0 and 9:
-
+"""
 def has_function(value):
     sum_of_chars = 0
     for char in value:
@@ -7,3 +7,22 @@ def has_function(value):
 
     return sum_of_chars % 10
 print(" 'Bob' has has code: ",has_function('Bob'))
+"""
+
+#According to our hash function, "Bob" should be stored at index 5.
+#Lets create a function that add items to our hash table:
+my_list = [None, None, None, None, None, None, None, None, None, None]
+
+def has_function(value):
+    sum_of_chars = 0
+    for char in value:
+        sum_of_chars +=ord(char)
+
+    return sum_of_chars % 10
+
+def add(name):
+    index= has_function(name)
+    my_list[index] = name
+
+add ('Bob')
+print(my_list)
