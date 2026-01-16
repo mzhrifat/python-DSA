d6e5666958f5bcf6c1dcbb6a83f684513634262e
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-16 23:45:16 +0600

    handling collision commit

--- Hash Tables with.py
+++ Hash Tables with.py
@@ -30,7 +30,7 @@ add('oggy')
 add('siri')
 add('ani')
 print(my_list)
-"""
+
 
 #lokking up a name
 
@@ -57,3 +57,41 @@ add('Jones')
 add('Lisa')
 add('Siri')
 print("'Pete' is in the Hash Table:", contains('Pete'))
+"""
+
+#Handling collision
+my_list = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  []
+]
+
+def hash_function(value):
+  sum_of_chars = 0
+  for char in value:
+    sum_of_chars += ord(char)
+
+  return sum_of_chars % 10
+
+def add(name):
+  index = hash_function(name)
+  my_list[index].append(name)
+
+def contains(name):
+  index = hash_function(name)
+  return my_list[index] == name
+
+add('Bob')
+add('Pete')
+add('Jones')
+add('Lisa')
+add('Siri')
+add('Stuart')
+print(my_list)
