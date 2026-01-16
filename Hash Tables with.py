f18df620579b15115d6f4436e2ba3a5bc7b868d3
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-16 23:43:59 +0600

    looking for a comment commit

--- Hash Tables with.py
+++ Hash Tables with.py
@@ -7,7 +7,7 @@ def has_function(value):
 
     return sum_of_chars % 10
 print(" 'Bob' has has code: ",has_function('Bob'))
-"""
+
 
 #According to our hash function, "Bob" should be stored at index 5.
 #Lets create a function that add items to our hash table:
@@ -30,3 +30,30 @@ add('oggy')
 add('siri')
 add('ani')
 print(my_list)
+"""
+
+#lokking up a name
+
+my_list = [None, None, None, None, None, None, None, None, None, None]
+
+def hash_function(value):
+  sum_of_chars = 0
+  for char in value:
+    sum_of_chars += ord(char)
+
+  return sum_of_chars % 10
+
+def add(name):
+  index = hash_function(name)
+  my_list[index] = name
+
+def contains(name):
+  index = hash_function(name)
+  return my_list[index] == name
+
+add('Bob')
+add('Pete')
+add('Jones')
+add('Lisa')
+add('Siri')
+print("'Pete' is in the Hash Table:", contains('Pete'))
