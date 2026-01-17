82dce1531c3428a011264681517f994f6780125f
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-17 23:51:13 +0600

    In-order Traversal of Binary Trees commit

--- Trees.py
+++ Trees.py
@@ -28,7 +28,7 @@ nodeF.left = nodeG
 
 # Test
 print("root.right.left.data:", root.right.left.data)
-"""
+
 
 # 2 Pre-order Traversal of Binary Trees
 
@@ -67,3 +67,41 @@ nodeF.left = nodeG
 
 # Traverse
 preOrderTraversal(root)
+"""
+
+#3 In-order Traversal of Binary Trees
+class TreeNode:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+
+def inOrderTraversal(node):
+  if node is None:
+    return
+  inOrderTraversal(node.left)
+  print(node.data, end=", ")
+  inOrderTraversal(node.right)
+    
+root = TreeNode('R')
+nodeA = TreeNode('A')
+nodeB = TreeNode('B')
+nodeC = TreeNode('C')
+nodeD = TreeNode('D')
+nodeE = TreeNode('E')
+nodeF = TreeNode('F')
+nodeG = TreeNode('G')
+
+root.left = nodeA
+root.right = nodeB
+
+nodeA.left = nodeC
+nodeA.right = nodeD
+
+nodeB.left = nodeE
+nodeB.right = nodeF
+
+nodeF.left = nodeG
+
+# Traverse
+inOrderTraversal(root)
