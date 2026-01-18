7c7e1e9dab5c70e18e635f82aff08b1f303ea0c4
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-18 23:45:19 +0600

    Find the lowest value in a BST subtree commit

--- Binary search Trees.py
+++ Binary search Trees.py
@@ -84,8 +84,8 @@ if result:
 else:
   print("Value not found in the BST.")
 
-"""
-  #Inserting new value into the BST
+
+# Inserting new value into the BST
 
 class TreeNode:
   def __init__(self, data):
@@ -135,5 +135,53 @@ insert(root, 10)
 
 # Traverse
 inOrderTraversal(root)
+
+"""
+
+#4 Find the lowest value in a BST subtree
+class TreeNode:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+
+def inOrderTraversal(node):
+  if node is None:
+    return
+  inOrderTraversal(node.left)
+  print(node.data, end=", ")
+  inOrderTraversal(node.right)
+
+def minValueNode(node):
+  current = node
+  while current.left is not None:
+    current = current.left
+  return current
+
+root = TreeNode(13)
+node7 = TreeNode(7)
+node15 = TreeNode(15)
+node3 = TreeNode(3)
+node8 = TreeNode(8)
+node14 = TreeNode(14)
+node19 = TreeNode(19)
+node18 = TreeNode(18)
+
+root.left = node7
+root.right = node15
+
+node7.left = node3
+node7.right = node8
+
+node15.left = node14
+node15.right = node19
+
+node19.left = node18
+
+# Traverse
+inOrderTraversal(root)
+
+# Find Lowest
+print("\nLowest value:",minValueNode(root).data)
 
 
