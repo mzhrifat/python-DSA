31e9c26366f3c8485ff518769d7a13fe454df0ea
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-20 23:40:27 +0600

    index of value commit

--- LInear search.py
+++ LInear search.py
@@ -1,7 +1,28 @@
 #Check if a value exists in a list:
+"""
 mylist= [3,7,2,9,5,1,8,4,6]
 
 if 4 in mylist:
     print("Found!")
 else:
     print("Not Found!")
+"""
+
+#2 Find the index of a value in a list:
+
+def linearSearch(arr,targetVAl):
+    for i in range (len(arr)):
+
+        if arr[i]== targetVAl:
+            return -1
+mylist = [3,7,2,9,5,1,8,4,6]
+x=4
+
+result = linearSearch(mylist,x)
+
+if result != -1:
+    print("Found  at index",result)
+ 
+else:
+    print("Not found")
+
