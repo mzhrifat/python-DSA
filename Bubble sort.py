b9050373e9d68bdad5fe789bbe825c1bcf83b9b2
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-20 23:52:52 +0600

    improved Bubble sort algorithm comit

--- Bubble sort.py
+++ Bubble sort.py
@@ -9,3 +9,19 @@ for i in range(n-1):
       mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
 
 print(mylist)
+
+
+#2 Improved Bubble Sort algorithm:
+mylist = [7, 3, 9, 12, 11]
+
+n = len(mylist)
+for i in range(n-1):
+  swapped = False
+  for j in range(n-i-1):
+    if mylist[j] > mylist[j+1]:
+      mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+      swapped = True
+  if not swapped:
+    break
+
+print(mylist)
