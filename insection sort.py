e28abf636c6c02f02326a638ac348fd85df0ed4e
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-21 23:23:37 +0600

    insection sort algorithm commit

--- insection sort.py
+++ insection sort.py
@@ -1,5 +1,5 @@
 #Using the Insertion Sort on a Python list:
-
+"""
 mylist = [64, 34, 25, 12, 22, 11, 90, 5]
 
 n = len(mylist)
@@ -10,5 +10,23 @@ for i in range(1,n):
     if mylist[j] > current_value:
       insert_index = j
   mylist.insert(insert_index, current_value)
+
+print(mylist)
+"""
+
+#Insert the improvements in the sorting algorithm:
+mylist = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(mylist)
+for i in range(1,n):
+  insert_index = i
+  current_value = mylist[i]
+  for j in range(i-1, -1, -1):
+     if mylist[j] > current_value:
+       mylist[j+1] = mylist[j]
+       insert_index = j
+     else:
+       break
+  mylist[insert_index] = current_value
 
 print(mylist)
